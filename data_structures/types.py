087d601beaabafea2cb87436f72e64b0b99e31ee
Kevin Solorio <soloriok@gmail.com>
2020-05-04 19:03:03 -0500

    add initial tests for point and vector

--- data_structures/types.py
+++ data_structures/types.py
@@ -9,3 +9,9 @@ class Coordinates:
     y: float
     z: float
     w: float
+
+    def is_point(self):
+        return self.w == float(1)
+
+    def is_vector(self):
+        return self.w == float(0)
--- tests/data_structures/test_types.py
+++ tests/data_structures/test_types.py
@@ -1,11 +1,21 @@
 from data_structures.types import Coordinates
 
 
-class TestCoordinatesInitializations:
-    def test_sets_x_y_z_w(self):
+class TestCoordinates:
+    def test_initialization(self):
         coordinates = Coordinates(x=1, y=2, z=3, w=4)
 
         assert coordinates.x == 1
         assert coordinates.y == 2
         assert coordinates.z == 3
         assert coordinates.w == 4
+
+    def test_is_point(self):
+        coordinates = Coordinates(x=0, y=0, z=0, w=1)
+        assert coordinates.is_point()
+        assert not coordinates.is_vector()
+
+    def test_is_vector(self):
+        coordinates = Coordinates(x=0, y=0, z=0, w=0)
+        assert not coordinates.is_point()
+        assert coordinates.is_vector()

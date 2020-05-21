230e3a46c4a0f24db0d4ca1a6f2967793a4ec5d8
Kevin Solorio <soloriok@gmail.com>
2020-05-20 20:52:12 -0500

    test handles 3x3 and 2x2 matrices

--- tests/data_structures/test_matrices.py
+++ tests/data_structures/test_matrices.py
@@ -19,3 +19,19 @@ class TestMatrix:
         assert matrix.at(3, 0) == 13.5
         assert matrix.at(3, 2) == 15.5
 
+    def test_creating_2_by_2_matrix(self):
+        data = [[-3, 5], [1, -2]]
+        matrix = Matrix(data)
+
+        assert matrix.at(0, 0) == -3
+        assert matrix.at(0, 1) == 5
+        assert matrix.at(1, 0) == 1
+        assert matrix.at(1, 1) == -2
+
+    def test_creating_3_by_3_matrix(self):
+        data = [[-3, 5, 0], [1, -2, -7], [0, 1, 1]]
+        matrix = Matrix(data)
+
+        assert matrix.at(0, 0) == -3
+        assert matrix.at(1, 1) == -2
+        assert matrix.at(2, 1) == 1

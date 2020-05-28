88d806a20fec3ce14230f4d2cb31c5007a4592fe
Kevin Solorio <soloriok@gmail.com>
2020-05-28 16:21:24 -0500

    tranlation working

--- drawing/transformations.py
+++ drawing/transformations.py
@@ -1,2 +1,5 @@
+from data_structures import Matrix
+
+
 def translation(x, y, z):
-    pass
+    return Matrix([[1, 0, 0, x], [0, 1, 0, y], [0, 0, 1, z], [0, 0, 0, 1]])
--- tests/drawing/test_transformations.py
+++ tests/drawing/test_transformations.py
@@ -7,7 +7,7 @@ class TestTranslation:
         transform = translation(5, -3, 2)
         point = Coordinates.point(-3, 4, 5)
 
-        result = tranform * point
+        result = transform * point
 
         assert result == Coordinates.point(2, 1, 7)
 
@@ -19,3 +19,11 @@ class TestTranslation:
         result = inverse * point
 
         assert result == Coordinates.point(-8, 7, 3)
+
+    def test_translation_does_not_affect_vectors(self):
+        transform = translation(5, -3, 2)
+        vector = Coordinates.vector(-3, 4, 5)
+
+        result = transform * vector
+
+        assert result == vector

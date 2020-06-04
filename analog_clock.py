425cc08bbe3fed1ab0dd81720be88312d4c0c6ea
Kevin Solorio <soloriok@gmail.com>
2020-06-03 21:59:48 -0500

    starting clock problem one step at a time

--- analog_clock.py
+++ analog_clock.py
@@ -2,29 +2,21 @@ from math import radians
 
 from drawing import Canvas
 from drawing.formats import PPMFormat
-from drawing.transformations import translation, rotation_z
+from drawing.transformations import translation, rotation_z, rotation_y, rotation_x
 from data_structures import Coordinates, Color
 
 width = 900
 height = 900
 
 canvas = Canvas(width, height)
-point = Coordinates.point(width / 2, height / 2, 0)
+point = Coordinates.point(int(width / 2), int(height / 2), 0)
 degrees_of_rotations = 360 / 12
 radius = (height / 2) * 0.8
 white = Color(1, 1, 1)
+translate = translation(0, -radius, 0)
 
-for i in range(12):
-    translate = translation(0, radius, 0)
-    rotations = rotation_z(radians(int(i * degrees_of_rotations)))
-
-    pixel_location = [
-        int(coordinate) for coordinate in (rotations * (translate * point))
-    ]
-    import pdb
-
-    pdb.set_trace()
-
-    canvas.write_pixel(pixel_location[0], pixel_location[1], white)
+canvas.write_pixel(point.x, point.y, white)
+tranlated_point = translate * point
+canvas.write_pixel(int(tranlated_point.x), int(tranlated_point.y), white)
 
 PPMFormat(canvas).write()

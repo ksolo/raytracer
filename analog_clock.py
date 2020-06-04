b9e869662ba305c32ea3a67878786997050d4201
Kevin Solorio <soloriok@gmail.com>
2020-06-03 22:07:53 -0500

    getting closer...maybe?

--- analog_clock.py
+++ analog_clock.py
@@ -9,14 +9,18 @@ width = 900
 height = 900
 
 canvas = Canvas(width, height)
-point = Coordinates.point(int(width / 2), int(height / 2), 0)
 degrees_of_rotations = 360 / 12
 radius = (height / 2) * 0.8
 white = Color(1, 1, 1)
+
+point = Coordinates.point(int(width / 2), int(height / 2), 0)
 translate = translation(0, -radius, 0)
 
-canvas.write_pixel(point.x, point.y, white)
-tranlated_point = translate * point
-canvas.write_pixel(int(tranlated_point.x), int(tranlated_point.y), white)
+for i in range(12):
+    rotate = rotation_z(radians(i * degrees_of_rotations))
+    rotated_point = rotate * (translate * point)
+
+    canvas.write_pixel(int(rotated_point.x), int(rotated_point.y), white)
+
 
 PPMFormat(canvas).write()

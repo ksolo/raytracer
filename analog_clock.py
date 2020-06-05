326f6765077d796019a705182ad037db708a445b
Kevin Solorio <soloriok@gmail.com>
2020-06-04 21:48:18 -0500

    still not there, but will come back to this later

--- analog_clock.py
+++ analog_clock.py
@@ -11,16 +11,20 @@ height = 900
 canvas = Canvas(width, height)
 degrees_of_rotations = 360 / 12
 radius = (height / 2) * 0.8
-white = Color(1, 1, 1)
 
-point = Coordinates.point(int(width / 2), int(height / 2), 0)
-translate = translation(0, -radius, 0)
+point = Coordinates.point(0, 0, 0)
+translate = translation(width / 2, height / 2, 0)
 
 for i in range(12):
-    rotate = rotation_z(radians(i * degrees_of_rotations))
+    colors = [Color(1, 0, 0), Color(0, 1, 0), Color(0, 0, 1)]
+    degrees = i * degrees_of_rotations
+    print(f"degrees: {degrees}")
+    rotate = rotation_z(radians(degrees))
     rotated_point = rotate * (translate * point)
 
-    canvas.write_pixel(int(rotated_point.x), int(rotated_point.y), white)
+    print(f"rotated_point: {rotated_point.x} {rotated_point.y} {rotated_point.z}")
+
+    canvas.write_pixel(int(rotated_point.x), int(rotated_point.y), colors[i % 3])
 
 
 PPMFormat(canvas).write()
